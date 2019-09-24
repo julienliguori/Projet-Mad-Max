d783e920fb516f4a75f646bfd7753a48e5ed7ab3
julienliguori <33621048+julienliguori@users.noreply.github.com>
2019-09-24 10:55:11 +0200

    Decrypte

--- src/Modèles/Decrypt.java
+++ src/Modèles/Decrypt.java
@@ -2,6 +2,13 @@ package Modèles;
 
 public class Decrypt {
 	String decrypt(String data, String key) {
-		
+		String chaineCryptee = "";
+	    for(int i = 0, j = 0; i < data.length(); i++)
+	    {
+	        chaineCryptee += (char)((int)data.charAt(i) ^ (int)key.charAt(j));
+	        j++;
+	        if(j >= key.length()) j = 0;
+	    }
+	    return chaineCryptee;
 	}
 }
--- src/Modèles/Files.java
+++ src/Modèles/Files.java
@@ -1,10 +1,32 @@
 package Modèles;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Files {
 	String getData(String path) {
-		
+		String ligne = "";
+		try{
+			InputStream flux=new FileInputStream(path); 
+			InputStreamReader lecture=new InputStreamReader(flux);
+			BufferedReader buff=new BufferedReader(lecture);
+			while ((ligne=buff.readLine())!=null){
+				return ligne;
+			}
+			buff.close(); 
+			}		
+			catch (Exception e){
+			System.out.println(e.toString());
+			}
+		return ligne;
 	}
-	void setData(String path) {
-		
+	void setData(String path, String texte) throws IOException {
+		FileWriter fichier = new FileWriter(path);
+	    fichier.write (texte);
+	    fichier.close();
 	}
 }
