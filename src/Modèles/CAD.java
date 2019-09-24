11500d3415db9e73733a47c6098cd78959e790c0
dz <dz@macbook-pro-de-dz.local>
2019-09-24 19:33:59 +0200

    CAD.java

--- src/Modèles/CAD.java
+++ src/Modèles/CAD.java
@@ -1,13 +1,49 @@
-package Modèles;
+package ModÃ¨les;
 
-import javax.xml.crypto.Data;
+import java.sql.*;
 
 public class CAD {
+	
+    private  String DB_DRIVER = "com.mysql.jdbc.Driver";
+	private  String DB_HOST = "jdbc:mysql://localhost/";
+    private  String DB_USER = "root";
+    private  String DB_PASSWORD = "root";
+    
+    private Connection cnx;
+    private Statement stm;
+  
+    public CAD() {
+		try {
+			Class.forName(DB_DRIVER);
+			this.cnx = DriverManager.getConnection(DB_HOST,DB_USER,DB_PASSWORD);
+			this.stm = this.cnx.createStatement();
+			
+			
+		} catch(SQLException | ClassNotFoundException e) {
+			
+		}
+	}
+    
+    
+    
+   
 
-	Data GetRows(String rq_sql,String resultSetName) {
+	public ResultSet GetRows(String rq_sql,String resultSetName)  {
+		try {
+		return this.stm.executeQuery(rq_sql);
+	} catch(SQLException e){
+		return null;
+		
+	}
 		
 	}
 	void ActionRows(String rq_sql) {
+		try {
+			this.stm.executeUpdate(rq_sql);
+		} catch(SQLException e){
+			
+			
+		}
 		
 	}
 }
